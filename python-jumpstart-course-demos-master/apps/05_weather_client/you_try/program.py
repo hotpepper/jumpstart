d4e1695a687d49e951bc2299ff23205ef483a742
hotpepper <aasere@gmail.com>
2017-08-07 22:24:15 -0400

    adds starting structure to weather app

---
+++ python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
@@ -0,0 +1,20 @@
+
+
+def main():
+    # print the header
+    print_header()
+    # get zipcode from user
+    zipcode = input('What zipcode do you want the weather for (11372)?\n')
+    # get html from web
+    # parse html
+    # display the forcast
+
+
+def print_header():
+    print('------------------------------------')
+    print('          WEATHER APP')
+    print('------------------------------------')
+
+
+if __name__ == '__main__':
+    main()
