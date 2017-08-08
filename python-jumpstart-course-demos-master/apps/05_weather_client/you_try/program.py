2e69a93b599316f7cce0f69b7e4264b3058c5fc4
hotpepper <aasere@gmail.com>
2017-08-07 23:00:51 -0400

    get html from url todo: parse html with beautiful soup

--- python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
+++ python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
@@ -1,3 +1,4 @@
+import requests
 
 
 def main():
@@ -6,6 +7,7 @@ def main():
     # get zipcode from user
     zipcode = input('What zipcode do you want the weather for (11372)?\n')
     # get html from web
+    html = get_html_fom_web(zipcode)
     # parse html
     # display the forcast
 
@@ -14,6 +16,14 @@ def print_header():
     print('------------------------------------')
     print('          WEATHER APP')
     print('------------------------------------')
+
+
+def get_html_fom_web(zipcode):
+    url = 'https://www.wunderground.com/us/{}'.format(zipcode)
+    response = requests.get(url)
+    # print(response.status_code)
+    # print(response.text[:250])
+    return response.text
 
 
 if __name__ == '__main__':
