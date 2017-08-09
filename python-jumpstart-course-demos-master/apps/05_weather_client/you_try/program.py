22442fd6fb86d46fb19b846fc68ca9d0ab9ac625
hotpepper <aasere@gmail.com>
2017-08-08 21:41:24 -0400

    start to parse html with beautiful soup

--- python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
+++ python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
@@ -1,4 +1,5 @@
 import requests
+import bs4
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     # get html from web
     html = get_html_fom_web(zipcode)
     # parse html
+    get_weather_from_html(html)
     # display the forcast
 
 
@@ -25,6 +27,19 @@ def get_html_fom_web(zipcode):
     # print(response.text[:250])
     return response.text
 
+
+def get_weather_from_html(html):
+    cityCss = 'div#location h1'
+    weatherConditionCss = 'div#curCond span.wx-value'
+    weatherTempCss = 'div#curtemp span.wx-data span.wx-value'
+    weatherScaleCs = 'div#curtemp span.wx-data span.wx-unit'
+
+    soup = bs4.BeautifulSoup(html, 'html5lib')
+    loc = soup.find(id='location').find('h1').get_text()
+    condition = soup.find(id='curCond').find(class_='wx-value').get_text()
+    temp = soup.find(id='curTemp').find(class_='wx-value').get_text()
+    scale = soup.find(id='curTemp').find(class_='wx-unit').get_text()
+    print(condition, temp, scale)
 
 if __name__ == '__main__':
     main()
