45db6d76a8dff29bcc0ff226ed2425faddb1eea8
hotpepper <aasere@gmail.com>
2017-08-08 22:03:48 -0400

    adds print report with named tupels

--- python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
+++ python-jumpstart-course-demos-master/apps/05_weather_client/you_try/program.py
@@ -1,5 +1,9 @@
 import requests
 import bs4
+import collections
+
+WeatherReport = collections.namedtuple('WeatherReport',
+                                       'cond, temp, scale, loc')
 
 
 def main():
@@ -10,8 +14,11 @@ def main():
     # get html from web
     html = get_html_fom_web(zipcode)
     # parse html
-    get_weather_from_html(html)
+    report = get_weather_from_html(html)
     # display the forcast
+    print ('The temp in {} is {}{} and {}'.format(
+        report.loc, report.temp, report.scale, report.cond
+    ))
 
 
 def print_header():
@@ -35,6 +42,10 @@ def clean_up_text(text : str): # (param : type) => type hints to give vaiable me
     return text
 
 
+def find_city_state_from_loc(loc : str):
+    return loc.split('\n')[0].strip()
+
+
 def get_weather_from_html(html):
     cityCss = 'div#location h1'
     weatherConditionCss = 'div#curCond span.wx-value'
@@ -48,7 +59,14 @@ def get_weather_from_html(html):
     scale = soup.find(id='curTemp').find(class_='wx-unit').get_text()
     
     loc = clean_up_text(loc)
-    print(condition, temp, scale)
+    loc = find_city_state_from_loc(loc)
+    condition = clean_up_text(condition)
+    temp = clean_up_text(temp)
+    scale = clean_up_text(scale)
+
+    # return condition, temp, scale, loc
+    report = WeatherReport(cond=condition, temp=temp, scale=scale, loc=loc)
+    return report
 
 if __name__ == '__main__':
     main()
